b1abb5c997d290236052872e3d4b7ad2bead9b16
James Chen <jwc221@ic.ac.uk>
2024-07-12 01:10:21 +0100

    w

--- linearstabilitycontinuation.m
+++ linearstabilitycontinuation.m
@@ -2,7 +2,7 @@ global d
 global f
 global new_x
 
-data = load('f0_200_f1_240_d_88_df_1_phase_5.000000e-01_ndts_400.mat')
+data = load('f0_200_f1_300_d_88_df_1_phase_5.000000e-01_ndts_400.mat')
 
 % save("continuationinputantiphase.mat", "d", "f0", "f1", "numf", "new_x")
 continuationarray = data.continuationarray;
@@ -12,6 +12,7 @@ f0 = data.f0;
 f1 = data.f1;
 df = data.df;
 numf = (f1-f0)/df;
+ndts = data.ndts;
 
 evalarray = zeros(numf+1, 6);
 evecarray = zeros(numf+1, 114, 6);
@@ -20,7 +21,7 @@ parfor i=1:numf+1
     f = f0+df*(i-1);
     new_x = continuationarray(:, i);
     fprintf('floquet: starting iteration with f=%i\n',f) ;
-    [eval, evec] = LinearStability(f, d, new_x(1), new_x(2:end), 200);
+    [eval, evec] = LinearStability(f, d, new_x(1), new_x(2:end), ndts);
     evalarray(i, :) = eval;
     evecarray(i, :, :) = evec;
 end
