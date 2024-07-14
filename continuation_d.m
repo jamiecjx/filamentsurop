8c89a169949c74fab0f216a01b58f4ba27a8f946
James Chen <jwc221@ic.ac.uk>
2024-07-15 00:48:37 +0100

    got damn thihaw

--- continuation_d.m
+++ continuation_d.m
@@ -23,21 +23,25 @@ function continuation_d(i)
     dd = (d1-d0)/numd;
     
     new_x = continuationarray(:,i);
-    
+
     continuationarray3d = zeros(size(new_x, 1), numd+1);
     size(continuationarray3d)
     M = numf+1
     L = numd+1
     
+    save(sprintf('f_%i_d0_%i_d1_%i_df_%i_dd_%i_phase_%i_ndts_%i.mat',f,d0,d1,df,dd,phase,ndts), ...
+        "continuationarray3d", "f0", "f1", "d", "df", "phase", "ndts");
+
     f = f0+df*(i-1);
     for j=1:L
         d = d0+dd*(j-1);
         fprintf('continuation: starting iteration with f=%i, d=%i',f,d) ;
         JFNK
         continuationarray3d(:,j) = new_x;
+        save(sprintf('f_%i_d0_%i_d1_%i_df_%i_dd_%i_phase_%i_ndts_%i.mat',f,d0,d1,df,dd,phase,ndts), ...
+        "continuationarray3d", "f0", "f1", "d", "df", "phase", "ndts");
     end
     
     
-    save(sprintf('f_%i_d0_%i_d1_%i_df_%i_dd_%i_phase_%i_ndts_%i.mat',f,d0,d1,df,dd,phase,ndts), ...
-        "continuationarray3d", "f0", "f1", "d", "df", "phase", "ndts");
+    
 end
