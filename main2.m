fba9500c732031dc83c5d452c84fd14e29fcc652
James Chen <jwc221@ic.ac.uk>
2024-07-17 01:25:36 +0100

    fh

--- main2.m
+++ main2.m
@@ -16,7 +16,7 @@ function [EffLieAlgebra] = main2
 %         vid = 1 to plot filament and save video, 0 otherwise
 
 % Variables to get started:
-f = 44;
+f = 40;
 u = zeros(57,2);
 % u(2:3:end, :) = 10^-1*randn(19,2);
 u(2:3:end, 2) = 10^-2*randn(19,1);
