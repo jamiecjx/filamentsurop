52132459434d0aa4cc9791f43e0a11c07d9597c3
James Chen <mrjameschen03@gmail.com>
2024-08-02 13:52:34 +0100

    kg2

--- merge.m
+++ merge.m
@@ -16,34 +16,37 @@
 %     "continuationarray", "f0", "f1", "d", "df", "phase", "ndts");
 % end
 
-for d=66:88
-    w1 = load(sprintf('f0_41_f1_200_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
-    delete(sprintf('f0_41_f1_200_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
-    cont1 = w1.continuationarray;
-    df = 1;
-    f0 = 41;
-    f1 = 300;
-    ndts = 400;
-    phase = 0.5;
-    continuationarray = w1.continuationarray;
-    save(sprintf('f0_%i_f1_%i_d_%i_df_%i_phase_%i_ndts_%i.mat',f0,f1,d,df,phase,ndts), ...
-    "continuationarray", "f0", "f1", "d", "df", "phase", "ndts");
-end
-% continuationarray = zeros(115, 101, 23);
 % for d=66:88
-%     w1 = load(sprintf('f0_200_f1_300_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
-%     continuationarray(:, :, 89-d) = w1.continuationarray;
+%     w1 = load(sprintf('f0_41_f1_200_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
+%     delete(sprintf('f0_41_f1_200_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
+%     cont1 = w1.continuationarray;
+%     df = 1;
+%     f0 = 41;
+%     f1 = 300;
+%     ndts = 400;
+%     phase = 0.5;
+%     continuationarray = w1.continuationarray;
+%     save(sprintf('f0_%i_f1_%i_d_%i_df_%i_phase_%i_ndts_%i.mat',f0,f1,d,df,phase,ndts), ...
+%     "continuationarray", "f0", "f1", "d", "df", "phase", "ndts");
 % end
-% d0=88
-% d1=66
-% f0=200
-% f1=300
-% df=1
-% dd=-1
-% phase=0.5
-% ndts=400
-% save(sprintf('2d_f0_%i_f1_%i_df_%i_d0_%i_d1_%i_dd_%i_phase_%i_ndts_%i.mat', ...
-%     f0, f1, df, d0, d1, dd, phase, ndts), "continuationarray", 'f0', 'f1', 'df', 'd0', 'd1', 'dd', 'phase', 'ndts')
+
+
+
+continuationarray = zeros(115, 260, 23);
+for d=66:88
+    w1 = load(sprintf('f0_41_f1_300_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
+    continuationarray(:, :, 89-d) = w1.continuationarray;
+end
+d0=88
+d1=66
+f0=41
+f1=300
+df=1
+dd=-1
+phase=0.5
+ndts=400
+save(sprintf('2d_f0_%i_f1_%i_df_%i_d0_%i_d1_%i_dd_%i_phase_%i_ndts_%i.mat', ...
+    f0, f1, df, d0, d1, dd, phase, ndts), "continuationarray", 'f0', 'f1', 'df', 'd0', 'd1', 'dd', 'phase', 'ndts')
 
 
 % w1 = load('2d_f0_41_f1_100_df_1_d0_88_d1_66_dd_-1_phase_5.000000e-01_ndts_400.mat');
