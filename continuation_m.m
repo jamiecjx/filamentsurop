c824926e2840c994ca285b4c72878fd2c8697d2c
James Chen <mrjameschen03@gmail.com>
2024-08-08 23:08:58 +0100

    hw

--- continuation_m.m
+++ continuation_m.m
@@ -1,12 +1,12 @@
 function continuation_m(i)
     d = 89-i
-    data = load(sprintf('f0_41_f1_50_d_%i_df_1_phase_0_ndts_400.mat', d));
+    data = load(sprintf('f0_50_f1_100_d_%i_df_1_phase_0_ndts_400.mat', d));
     continuationarray = data.continuationarray;
     phase=data.phase;
     ndts=data.ndts;
-    f0=50;
-    f1=100;
-    numf=50;
+    f0=100;
+    f1=200;
+    numf=100;
     new_x = continuationarray(:, end);
 
     save(sprintf("continuationinput_%i.mat", i), "d", "f0", "f1", "numf", "new_x", "ndts", "phase") 
