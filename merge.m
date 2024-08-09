5e8836235c7219c77c1ff6af32ad7d8bdcad4b06
James Chen <mrjameschen03@gmail.com>
2024-08-09 11:59:40 +0100

    oop forgot this

--- merge.m
+++ merge.m
@@ -1,15 +1,20 @@
 % for d=66:88
-%     w1 = load(sprintf('f0_41_f1_200_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
-%     w2 = load(sprintf('f0_200_f1_300_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
-%     delete(sprintf('f0_41_f1_200_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
-%     delete(sprintf('f0_200_f1_300_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
+%     file1='f0_41_f1_50_d_%i_df_1_phase_0_ndts_400.mat'
+%     file2='f0_50_f1_100_d_%i_df_1_phase_0_ndts_400.mat'
+%     w1 = load(sprintf(file1, d));
+%     w2 = load(sprintf(file2, d));
+%     delete(sprintf(file1, d));
+%     delete(sprintf(file2, d));
 %     cont1 = w1.continuationarray;
 %     cont2 = w2.continuationarray;
+% 
 %     df = 1;
 %     f0 = 41;
-%     f1 = 200;
+%     f1 = 100;
 %     ndts = 400;
-%     phase = 0.5;
+%     phase = 0;
+% 
+% 
 %     norm(cont1(:, end) - cont2(:, 1))
 %     continuationarray = [cont1(:, 1:end-1), cont2];
 %     save(sprintf('f0_%i_f1_%i_d_%i_df_%i_phase_%i_ndts_%i.mat',f0,f1,d,df,phase,ndts), ...
@@ -33,18 +38,18 @@
 
 
 
-continuationarray = zeros(115, 101, 23);
+continuationarray = zeros(115, 60, 23);
 for d=66:88
-    w1 = load(sprintf('f0_300_f1_400_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
+    w1 = load(sprintf('f0_41_f1_100_d_%i_df_1_phase_0_ndts_400.mat', d));
     continuationarray(:, :, 89-d) = w1.continuationarray;
 end
 d0=88
 d1=66
-f0=300
-f1=400
+f0=41
+f1=100
 df=1
 dd=-1
-phase=0.5
+phase=0
 ndts=400
 save(sprintf('2d_f0_%i_f1_%i_df_%i_d0_%i_d1_%i_dd_%i_phase_%i_ndts_%i.mat', ...
     f0, f1, df, d0, d1, dd, phase, ndts), "continuationarray", 'f0', 'f1', 'df', 'd0', 'd1', 'dd', 'phase', 'ndts')
@@ -56,3 +61,30 @@ save(sprintf('2d_f0_%i_f1_%i_df_%i_d0_%i_d1_%i_dd_%i_phase_%i_ndts_%i.mat', ...
 % continuationarray = zeros(115, 260, 23);
 % continuationarray(:, 1:60, :) = w1;
 % continuationarray(:, 61:160) = 
+
+
+
+
+
+
+
+
+
+% evalarray = zeros(100, 23, 6);
+% 
+% for f=301:400
+%     data = load(sprintf("ls_f_%i_df_1_d0_88_d1_66_dd_-1_phase_5.000000e-01_ndts_400.mat", f));
+%     evalarraytemp = data.evalarray;
+%     evalarray(f-300, :, :) = evalarraytemp;
+% end
+% d0=88
+% d1=66
+% f0=301
+% f1=400
+% df=1
+% dd=-1
+% phase=0.5
+% ndts=400
+% 
+% save(sprintf('ls_f0_%i_f1_%i_df_%i_d0_%i_d1_%i_dd_%i_phase_%i_ndts_%i.mat',f0,f1,df,d0,d1,dd,phase,ndts), ...
+%         "evalarray", "f0", "f1", "d0", "d1", "dd", "ndts", "phase", "df");
--- plotevaltest.m
+++ plotevaltest.m
@@ -1,5 +1,6 @@
-[X,Y] = meshgrid(88:-1:66, 41:100);
-
+[X,Y] = meshgrid(88:-1:66, 301:400);
+hold on
 surf(X, Y, real(evalarray(:, :, 1)))
+surf(X, Y, real(evalarray(:, :, 2)))
 
 set(gca, 'YDir','reverse')
