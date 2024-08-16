f472f793f9b3901af0ce54710ed953f4a23e8ac8
James Chen <mrjameschen03@gmail.com>
2024-08-16 19:23:03 +0100

    try to fix

--- continuation_m.m
+++ continuation_m.m
@@ -1,13 +1,13 @@
 function continuation_m(i)
     d = 89-i
-    data = load(sprintf('f0_400_f1_500_d_%i_df_1_phase_5.000000e-01_ndts_400.mat', d));
+    data = load(sprintf('f0_200_f1_300_d_%i_df_1_phase_0_ndts_400.mat', d));
     continuationarray = data.continuationarray;
     phase=data.phase;
     ndts=data.ndts;
-    f0=500;
-    f1=600;
-    numf=100;
-    new_x = continuationarray(:, end);
+    f0=250;
+    f1=300;
+    numf=50;
+    new_x = continuationarray(:, 51);
 
     save(sprintf("continuationinput_%i.mat", i), "d", "f0", "f1", "numf", "new_x", "ndts", "phase") 
 
