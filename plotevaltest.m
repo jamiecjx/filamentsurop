7aaca5c77dfc3ee8938ea329d9bdd8dccd71b2ed
James Chen <mrjameschen03@gmail.com>
2024-08-20 09:56:50 +0100

    ok

--- plotevaltest.m
+++ plotevaltest.m
@@ -1,4 +1,4 @@
-[X,Y] = meshgrid(88:-1:66, 41:400);
+[X,Y] = meshgrid(88:-1:66, f0:f1);
 hold on
 surf(X, Y, real(evalarray(:, :, 1)))
 surf(X, Y, real(evalarray(:, :, 2)))
@@ -6,9 +6,9 @@ surf(X, Y, real(evalarray(:, :, 2)))
 set(gca, 'YDir','reverse')
 
 
-% data = load('ls_f0_41_f1_300_df_1_d0_88_d1_66_dd_-1_phase_5.000000e-01_ndts_400.mat')
+% data = load('ls_f0_41_f1_400_df_1_d0_88_d1_66_dd_-1_phase_5.000000e-01_ndts_400.mat')
 % we1 = data.evalarray;
-% data2 = load('ls_f0_301_f1_400_df_1_d0_88_d1_66_dd_-1_phase_5.000000e-01_ndts_400.mat')
+% data2 = load('ls_f0_401_f1_500_df_1_d0_88_d1_66_dd_-1_phase_5.000000e-01_ndts_400.mat')
 % 
 % we2 = data2.evalarray;
 % size(we1)
@@ -20,9 +20,9 @@ set(gca, 'YDir','reverse')
 % dd = -1
 % df = 1
 % f0 = 41
-% f1=400
+% f1=500
 % ndts=400
 % phase=0.5
 % 
 % save(sprintf('ls_f0_%i_f1_%i_df_%i_d0_%i_d1_%i_dd_%i_phase_%i_ndts_%i.mat',f0,f1,df,d0,d1,dd,phase,ndts), ...
-%         "evalarray", "f0", "f1", "d0", "d1", "dd", "ndts", "phase", "df");
+        % "evalarray", "f0", "f1", "d0", "d1", "dd", "ndts", "phase", "df");
